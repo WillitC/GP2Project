acaab3471b7d3a698350db1e1e2317513eb9fba2
Olivierleone <fatecasual@gmail.com>
2024-11-21 02:59:50 -0500

    Merge conflict fix

--- Reference/Assets/Script/Game/FramerateCounter.cs
+++ Reference/Assets/Script/Game/FramerateCounter.cs
@@ -1,32 +1,26 @@
 using UnityEngine;
-using TMPro; 
-
+using TMPro;
 public class FramerateCounter : MonoBehaviour
 {
-    public TMP_Text uiText; 
-    public TMP_Text uiTextWorldSpace; 
-
+    public TMP_Text uiText;
+    public TMP_Text uiTextWorldSpace;
     private float deltaTime = 0.0f;
-
     void Update()
     {
         // Update the frame time for FPS calculation
         deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
-
         // Calculate the FPS value
         int fps = Mathf.CeilToInt(1.0f / deltaTime);
-
         // Update both the UI Text and World Space Text with the FPS value
         if (uiText != null)
         {
             // Update the FPS in the UI canvas
-            uiText.text = $"{fps} FPS"; 
+            uiText.text = $"{fps} FPS";
         }
-
         if (uiTextWorldSpace != null)
         {
             // Update the FPS in the world space
-            uiTextWorldSpace.text = $"{fps} FPS"; 
+            uiTextWorldSpace.text = $"{fps} FPS";
         }
     }
 }
