5da225975e8787d8a17b0ecdffa642b4afbbe8f8
WillitC <117682514+WillitC@users.noreply.github.com>
2024-11-26 17:51:41 -0500

    v3.312
    Bullet Impact

--- Reference/Assets/Script/AIScript/Bullet.cs
+++ Reference/Assets/Script/AIScript/Bullet.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
--- Reference/Assets/Script/BulletComponent.cs
+++ Reference/Assets/Script/BulletComponent.cs
@@ -6,6 +6,8 @@ public class BulletComponent : MonoBehaviour
 {
     public float bulletSpeed = 10f; // Speed of the bullet
 
+    public GameObject impactPrefab;
+
     void Start()
     {
         // Add an impulse force in the forward direction
@@ -20,9 +22,29 @@ public class BulletComponent : MonoBehaviour
         else
         {
             Destroy(gameObject, 1.5f);
-        }
-        
+        }      
 
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerHealth playerHealth = collision.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(10f);
+            }
+        }
+
+        GameObject sparks = Instantiate(impactPrefab, transform.position, transform.rotation);
+        GameObject vfx = sparks.transform.GetChild(0).gameObject;
+
+        Destroy(vfx, 0.15f);
+        Destroy(sparks, 3);
+
+        gameObject.SetActive(false);
+        Destroy(gameObject, 2); // Destroy bullet after hitting anything
     }
 }
 
